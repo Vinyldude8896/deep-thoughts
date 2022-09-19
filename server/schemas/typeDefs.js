bb59adf9632aa446371c88aebfc4430ea4d503cc
Kevin Reid <kevinnivek@me.com>
2022-09-19 12:27:51 -0400

    added stuff

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,18 @@ const { gql } = require('apollo-server-express');
 
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
   type Thought {
     _id: ID
@@ -11,14 +23,6 @@ const typeDefs = gql`
     username: String
     reactionCount: Int
     reactions: [Reaction]
-  }
-  type User {
-    _id: ID
-    username: String
-    email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
   }
 
   type Reaction {
@@ -29,11 +33,19 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
 
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 `;
 
